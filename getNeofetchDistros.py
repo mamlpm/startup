a58661c9fe6d688e37a9824f95973623757d1f98
m1k4n <mamlpm95@gmail.com>
2023-11-17 17:21:00 +0100

    initial commit

---
+++ getNeofetchDistros.py
@@ -0,0 +1,22 @@
+import os
+import re
+from random import random
+
+
+def getDistros():
+    neofetchHelp = os.popen("neofetch --help").read().replace("\t",
+                                                              "").replace("\n", "").replace(" ", "")
+    stringInitVariable = "ascii_distrodistroWhichDistro'sasciiarttoprintNOTE:"
+    distros = neofetchHelp[neofetchHelp.find(
+        stringInitVariable) + len(stringInitVariable):neofetchHelp.find("haveasciilogos")]
+    distroRx = re.findall('([^,]+)', distros)
+    distroRx[len(distroRx) - 1] = distroRx[len(distroRx) - 1][3:]
+    return distroRx
+
+
+def getRandDistro():
+    distros = getDistros()
+    return distros[round(random() * len(distros))]
+
+
+getDistros()
